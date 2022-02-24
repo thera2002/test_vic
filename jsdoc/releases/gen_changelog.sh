93e7726f6c05aad2b6b984da5a96442bbf08d9e3
Fabio Bettio <fabio@crs4.it>
2022-02-24 15:25:59 +0100

    Fixed bug

--- jsdoc/releases/gen_changelog.sh
+++ jsdoc/releases/gen_changelog.sh
@@ -3,3 +3,4 @@ CURR=$(git describe --abbrev=0 --tags)
 PREV=$(git describe --abbrev=0 --tags ${CURR}^)
 echo "### Commits $PREV $CURR" 
 git log --pretty=format:"- %s (%ci %cn)" ${PREV}...${CURR}
+git tag
