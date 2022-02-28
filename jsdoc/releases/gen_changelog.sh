1607df5c77e120912850abd7c6ddc374d4259e6a
Fabio Bettio <fabio@crs4.it>
2022-02-28 14:59:36 +0100

    Added current tag info

--- jsdoc/releases/gen_changelog.sh
+++ jsdoc/releases/gen_changelog.sh
@@ -4,6 +4,13 @@ readarray -t a <<<"$s"
 for i in "${!a[@]}" ; do 
   ! [[ ${a[$i]} =~ ^v[0-9]+\. ]] && unset -v 'a[$i]'
 done
+
+if [[ "${#a[@]}" -gt 0 ]] 
+then
+    echo "### Tag ${a[1]}"
+    git tag -l --format='%(contents)' "${a[1]}"
+fi
+
 if [[ "${#a[@]}" -gt 1 ]] 
 then
     CURR="${a[1]}"
